a29d7f6e670e3de44c4c8a45e004c6949d8027d2
Graham Home <grahamhome333@gmail.com>
2023-04-06 09:59:42 -0400

    Improvements to factor-finding algorithm & passing tests

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,10 @@ mod my_tests;
 mod tests;
 
 #[derive(Eq, PartialEq, Debug)]
-struct Factor(usize, usize);
+struct Square {
+    rows: usize,
+    cols: usize,
+}
 
 pub fn encrypt(input: &str) -> String {
     let normalized_input: String = normalize(input);
@@ -10,7 +13,7 @@ pub fn encrypt(input: &str) -> String {
         0 => String::new(),
         1 => input.to_string(),
         n => {
-            let Factor(cols,_) = find_nearest_factor(n).unwrap();
+            let Square { cols, rows } = find_nearest_factor(n).unwrap();
             let input_chunks = flat_chunks(normalized_input, cols);
             // TODO: Remove
             input_chunks
@@ -18,6 +21,45 @@ pub fn encrypt(input: &str) -> String {
             // TODO: Produce output chunks with missing chars spread out
             // TODO: Join chunks with space buffers
         }
+    }
+}
+
+/// Produce a lowercase alphanumeric version of an input string.
+fn normalize(input: &str) -> String {
+    input
+        .to_lowercase()
+        .chars()
+        .filter(|c| c.is_alphanumeric())
+        .collect()
+}
+
+/// Finds the factor of the nearest number to the input.
+/// Returns the factor and the distance.
+fn find_nearest_factor(mut input: usize) -> Option<Square> {
+    let mut candidate: Option<Square> = factor(input);
+    while !candidate.is_some() {
+        input += 1;
+        candidate = factor(input);
+    }
+    candidate.map(|c| (c))
+}
+
+/// Find a factor pair for the given number with the smallest distance, either 0 or 1.
+fn factor(input: usize) -> Option<Square> {
+    match input {
+        2 => Some(Square { cols: 2, rows: 2 }),
+        n => (1..=((input as f64).sqrt().floor()) as usize)
+            .rev()
+            .find_map(|c| {
+                if input % c == 0 && input / c - c <= 1_usize {
+                    Some(Square {
+                        rows: c,
+                        cols: input / c,
+                    })
+                } else {
+                    None
+                }
+            }),
     }
 }
 
@@ -30,41 +72,4 @@ fn padded_flat_chunks(input: String, cols: usize) -> String {
 fn flat_chunks(input: String, cols: usize) -> String {
     // TODO: Write me
     String::new()
-}
-
-/// Finds the factor of the nearest number to the input.
-/// Returns the factor and the distance.
-fn find_nearest_factor(mut input: usize) -> Option<Factor> {
-    let mut candidate: Option<Factor> = factor(input);
-    while !candidate.is_some() && input > 0 {
-        input -= 1;
-        candidate = factor(input);
-    }
-    candidate.map(|c| (c))
-}
-
-/// Produce a lowercase alphanumeric version of an input string.
-fn normalize(input: &str) -> String {
-    input
-        .to_lowercase()
-        .chars()
-        .filter(|c| c.is_alphanumeric())
-        .collect()
-}
-
-/// Find a factor pair for the given number with the smallest distance, either 0 or 1.
-fn factor(input: usize) -> Option<Factor> {
-    match input {
-        2 => Some(Factor(2,2)),
-        n => (1..=((input as f64).sqrt().floor()) as usize)
-            .filter_map(|c| {
-                if input % c == 0 && (input / c) - c <= 1_usize {
-                    Some(Factor(c, input / c))
-                } else {
-                    None
-                }
-            })
-            .min_by_key(|f| f.1 - f.0)
-    }
-
 }
--- src/my_tests.rs
+++ src/my_tests.rs
@@ -3,28 +3,36 @@ mod tests {
     use crate::*;
 
     #[test]
-    fn factors_work() {
-        assert_eq!(factor(20), Some(Factor(4, 5)));
-        assert_eq!(factor(15), None);
-        assert_eq!(factor(25), Some(Factor(5, 5)));
-        assert_eq!(factor(132), Some(Factor(11, 12)));
+    fn closest_factor_works() {
+        assert_eq!(find_nearest_factor(3), Some(Square { cols: 2, rows: 2 }));
+        assert_eq!(find_nearest_factor(5), Some(Square { cols: 3, rows: 2 }));
+        assert_eq!(find_nearest_factor(7), Some(Square { cols: 3, rows: 3 }));
+        assert_eq!(find_nearest_factor(8), Some(Square { cols: 3, rows: 3 }));
+        assert_eq!(find_nearest_factor(10), Some(Square { cols: 4, rows: 3 }));
+        assert_eq!(find_nearest_factor(11), Some(Square { cols: 4, rows: 3 }));
+        assert_eq!(find_nearest_factor(15), Some(Square { cols: 4, rows: 4 }));
     }
 
     #[test]
-    fn factors() {
+    fn factors_work() {
         assert_eq!(factor(0), None);
-        assert_eq!(factor(1), Some(Factor(1, 1)));
-        assert_eq!(factor(2), Some(Factor(2, 2)));
+        assert_eq!(factor(1), Some(Square { cols: 1, rows: 1 }));
+        assert_eq!(factor(2), Some(Square { cols: 2, rows: 2 }));
         assert_eq!(factor(3), None);
-        assert_eq!(factor(4), Some(Factor(2, 2)));
+        assert_eq!(factor(4), Some(Square { cols: 2, rows: 2 }));
         assert_eq!(factor(5), None);
-        assert_eq!(factor(6), Some(Factor(2,3)));
+        assert_eq!(factor(6), Some(Square { cols: 3, rows: 2 }));
         assert_eq!(factor(7), None);
         assert_eq!(factor(8), None);
-        assert_eq!(factor(9), Some(Factor(3,3)));
+        assert_eq!(factor(9), Some(Square { cols: 3, rows: 3 }));
         assert_eq!(factor(10), None);
+        assert_eq!(factor(11), None);
+        assert_eq!(factor(12), Some(Square { cols: 4, rows: 3 }));
+        assert_eq!(factor(15), None);
+        assert_eq!(factor(20), Some(Square { cols: 5, rows: 4 }));
+        assert_eq!(factor(25), Some(Square { cols: 5, rows: 5 }));
+        assert_eq!(factor(132), Some(Square { cols: 12, rows: 11 }));
     }
-
 
     #[test]
     fn normalize_works() {
